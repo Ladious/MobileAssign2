5a55d093d2945d396866b71adef1e265bebadadd
Ladious <xladious@gmail.com>
2015-12-12 16:20:00 +0800

    Particular Customer Details DB Retrieve - ok

--- app/src/main/java/com/example/jia/mobileassignment/CustomerDetails.java
+++ app/src/main/java/com/example/jia/mobileassignment/CustomerDetails.java
@@ -5,24 +5,20 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
-
 public class CustomerDetails extends AppCompatActivity {
     ListView listViewCustomer;
     List<Customer> caList;
@@ -32,20 +28,15 @@ public class CustomerDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        readCustomer();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        listViewCustomer = (ListView) findViewById(R.id.listView);
+        pDialog = new ProgressDialog(this);
+        caList = new ArrayList<>();
+        readCourse();
     }
 
-    private void readCustomer() {
+
+    private void readCourse() {
         try {
             // Check availability of network connection.
             ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -53,7 +44,7 @@ public class CustomerDetails extends AppCompatActivity {
             Boolean isConnected = networkInfo != null && networkInfo.isConnectedOrConnecting();
             if (isConnected) {
                 //new downloadCourse().execute(getResources().getString(R.string.get_course_url));
-                downloadCustomer(this, getResources().getString(R.string.get_course_url));
+                downloadCourse(this, getResources().getString(R.string.get_course_url));
             } else {
                 Toast.makeText(getApplication(), "Network is NOT available",
                         Toast.LENGTH_SHORT).show();
@@ -65,7 +56,7 @@ public class CustomerDetails extends AppCompatActivity {
         }
     }
 
-    private void downloadCustomer(Context context, String url) {
+    private void downloadCourse(Context context, String url) {
         //mPostCommentResponse.requestStarted();
         RequestQueue queue = Volley.newRequestQueue(context);
 
@@ -80,31 +71,28 @@ public class CustomerDetails extends AppCompatActivity {
                             //Clear list
                             caList.clear();
 
-                            for(int i=0; i < response.length();i++){
+                           // for(int i=0; i < response.length();i++){
+                                JSONObject courseResponse = (JSONObject) response.get(0);
+                                String id = courseResponse.getString("id");
+                                String name = courseResponse.getString("name");
+                                String address = courseResponse.getString("address");
+                                String phone = courseResponse.getString("phone");
+                                String representative = courseResponse.getString("representative");
+                                String contact = courseResponse.getString("contact");
+                                String position = courseResponse.getString("position");
+                                String email = courseResponse.getString("email");
 
-                                JSONObject customerResponse = (JSONObject) response.get(i);
-                                String id = customerResponse.getString("id");
-                                if(id.equals("1000")){
-                                    String name = customerResponse.getString("name");
-                                    String address = customerResponse.getString("address");
-                                    String phone = customerResponse.getString("phone");
-                                    String representative = customerResponse.getString("representative");
-                                    String contact = customerResponse.getString("contact");
-                                    String position = customerResponse.getString("position");
-                                    String email = customerResponse.getString("email");
-
-                                    Customer customer = new Customer();
-                                    customer.setName(name);
-                                    customer.setAddress(address);
-                                    customer.setPhone(phone);
-                                    customer.setRepresentative(representative);
-                                    customer.setContact(contact);
-                                    customer.setPosition(position);
-                                    customer.setEmail(email);
-                                    caList.add(customer);
-                                }
-
-                            }
+                                Customer customer = new Customer();
+                                customer.setId(id);
+                                customer.setName(name);
+                                customer.setAddress(address);
+                                customer.setPhone(phone);
+                                customer.setRepresentative(representative);
+                                customer.setContact(contact);
+                                customer.setPosition(position);
+                                customer.setEmail(email);
+                                caList.add(customer);
+                         //  }
                             loadCourse();
 
                             if (pDialog.isShowing())
@@ -129,7 +117,8 @@ public class CustomerDetails extends AppCompatActivity {
     private void loadCourse() {
         final CustomerAdapter adapter = new CustomerAdapter(this, caList);
         listViewCustomer.setAdapter(adapter);
-       // Toast.makeText(getApplicationContext(), "Count :" + caList.size(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Count :" + caList.size(), Toast.LENGTH_SHORT).show();
     }
+
 
 }
